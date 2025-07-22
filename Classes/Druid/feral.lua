1f56fe2f8d7039df08b15e8f98d81c91a4c07177
adenworker <36097404+adenworker@users.noreply.github.com>
2025-07-22 19:49:07 +0200

    Update feral.lua

--- Classes/Druid/feral.lua
+++ Classes/Druid/feral.lua
@@ -16,6 +16,8 @@ Feral.Spells = {
     Regrowth = 8936,
     Renewal = 108238,
     BearForm = 5487,
+    MarkOfTheWild = 1126,
+    Mangle = 33917,
 }
 
 Feral.Buffs = {
@@ -24,6 +26,7 @@ Feral.Buffs = {
     SurvivalInstincts = 61336,
     PredatorySwiftness = 69369,
     BearForm = 5487,
+    MarkOfTheWild = 1126,
 }
 
 Feral.priority = function()
@@ -41,8 +44,29 @@ Feral.priority = function()
     local debuffsOnMe = P4.AuraTracker:GetActiveDebuffTypes("player")
 
 
-    if (tContains(debuffsOnMe, P4.Debuff.Curse) or tContains(debuffsOnMe, P4.Debuff.Poison)) and P4.IsSpellReady(Feral.Spells.RemoveCorruption) then
+    --[[if (tContains(debuffsOnMe, P4.Debuff.Curse) or tContains(debuffsOnMe, P4.Debuff.Poison)) and P4.IsSpellReady(Feral.Spells.RemoveCorruption) then
         return Feral.Spells.RemoveCorruption
+    end]]
+
+    local debuffedUnit = P4.AuraTracker:GetUnitWithDebuff(P4.Debuff.Poison, P4.Debuff.Curse)
+    local dispelReady = P4.IsSpellReady(Feral.Spells.RemoveCorruption)
+    if debuffedUnit and not UnitIsUnit("focus", debuffedUnit) then
+            if dispelReady then
+                P4.log("Focus " .. tostring(debuffedUnit) .. " for dispel", P4.DEBUG)
+                return P4.MacroSystem:GetMacroIDForUnit(debuffedUnit)
+            end
+    end
+    if debuffedUnit and UnitIsUnit("focus", debuffedUnit) and dispelReady then
+      return Feral.Spells.RemoveCorruption
+    end
+
+    if not P4.AuraTracker:EveryoneHas(Feral.Buffs.MarkOfTheWild) then
+        P4.log("Lapka (someone does not have it)", P4.DEBUG)
+        return Feral.Spells.MarkOfTheWild
+    end
+
+    if hasFrenziedRegeneration then
+        return Feral.Spells.Mangle
     end
 
     if healthPercent < 70 then
@@ -50,6 +74,13 @@ Feral.priority = function()
             return Feral.Spells.Renewal
         elseif barkskinReady and not hasFrenziedRegeneration and not hasSurvivalInstincts then
             return Feral.Spells.Barkskin
+        end
+    end
+
+    if healthPercent <= 50 then -- 50% hp
+        if P4.IsItemReady(211879) then -- Algari Healing Potion
+            P4.log("HP POTION (<50%)", P4.DEBUG)
+            return P4.MacroSystem:GetMacroIDForMacro("HealingPotion")
         end
     end
 
